a9b6d447043c680a5dbdd588ca0931cc90796035
Lauren Datz <105828115+ladatz@users.noreply.github.com>
2024-02-09 10:39:09 -0500

    Add missing component governance files

--- tools/notice_generation.sh
+++ tools/notice_generation.sh
@@ -33,7 +33,7 @@ fi
 
 NOTICE_FILENAME="NOTICE"
 echo "Running cargo-about for NOTICE file generation..."
-cargo about generate --workspace devops/cg/about.hbs --config devops/cg/about.toml | sed -E 's/[ \t]+\r?$//' > $NOTICE_FILENAME
+cargo about generate --workspace tools/cg/about.hbs --config tools/cg/about.toml | sed -E 's/[ \t]+\r?$//' > $NOTICE_FILENAME
 
 if [ -z "$(git diff --name-only $NOTICE_FILENAME)" ] && [ -z "$(git ls-files --others --exclude-standard)"]
 then
